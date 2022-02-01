3b0f7825f19a95236309b510e62789d6cf44dfeb
OzoneAintGotShtOnMe <59840042+OzoneAintGotShtOnMe@users.noreply.github.com>
2022-02-01 17:02:37 -0600

    Update FootballPlayer.js

--- FootballPlayer.js
+++ FootballPlayer.js
@@ -1,8 +1,27 @@
 class FootballPlayer {
   constructor(
-
+    // Defines parameters:
+    name,
+    height,
+    weight,
+    number,
+    position,
+    years,
+    touchdowns,
+    interceptions,
+    yards,
+    rushyards
   ) {
-
+    // Define properties:
+    this.name = name;
+    this.height = height;
+    this.weight = weight;
+    this.years = years;
+    this.position = position;
+    this.touchdowns = touchdowns;
+    this.interceptions = interceptions;
+    this.yards = yards;
+    this.rushyards = rushyards;
   }
 }
 
