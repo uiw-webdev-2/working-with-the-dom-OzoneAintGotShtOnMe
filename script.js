b94e133037eafef3380c3609f4bc8f403978b68d
OzoneAintGotShtOnMe <59840042+OzoneAintGotShtOnMe@users.noreply.github.com>
2022-03-03 05:10:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ import FootballPlayer from "./FootballPlayer.js";
 
 const tomBrady = new FootballPlayer(
      "Tom Brady",
+     "August 3, 1977 15:00:00 PST",
      "6'4",
      225,
      12,
@@ -22,8 +23,17 @@ const content = `
      <figure class="backpack__image">
        <img src=${tomBrady.image} alt="" />
      </figure>
-     <h1 class="backpack__name"></h1>
+     <h1 class="backpack__name">${tomBrady.name}</h1>
+     <h2 class="backpack__birthday>${tomBrady.birthday}</h2>
      <ul class="backpack__features">
+          <li class="packprop backpack__volume">Height:<span>${tomBrady.height}</span></li>
+          <li class="packprop backpack__color">Weight:<span>${tomBrady.weight}</span></li>
+          <li class="packprop backpack__number">Number:<span>${tomBrady.number}</span></li>
+          <li class="packprop backpack__yrs">Years:<span>${tomBrady.height}</span></li>
+          <li class="packprop backpack__td">Touchdowns:<span>${tomBrady.touchdowns}</span></li>
+          <li class="packprop backpack__int">Interceptions:<span>${tomBrady.interceptions}</span></li>
+          <li class="packprop backpack__yards">Total Yards:<span>${tomBrady.yards}</span></li>
+          <li class="packprop backpack__rush">Rushing Yards:<span>${tomBrady.rushyards}</span></li>
      </ul>
    </article>
  `;
