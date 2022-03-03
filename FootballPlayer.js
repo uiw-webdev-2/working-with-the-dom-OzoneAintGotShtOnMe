5196291b0f0bc08178d53ced483c27cd0361104f
OzoneAintGotShtOnMe <59840042+OzoneAintGotShtOnMe@users.noreply.github.com>
2022-03-03 04:23:06 -0600

    Update FootballPlayer.js

--- FootballPlayer.js
+++ FootballPlayer.js
@@ -2,6 +2,7 @@ class FootballPlayer {
   constructor(
     // Defines parameters:
     name,
+    birthday,
     height,
     weight,
     number,
@@ -14,6 +15,7 @@ class FootballPlayer {
   ) {
     // Define properties:
     this.name = name;
+    this.birthday = birthday;
     this.height = height;
     this.weight = weight;
     this.years = years;
@@ -23,6 +25,12 @@ class FootballPlayer {
     this.yards = yards;
     this.rushyards = rushyards;
   }
+  footballAge() {
+    let now = new Date();
+    let birthday = new Date(this.birthday);
+    let elapsed = now - acquired;
+    let daysSinceBorn = Math.floor(elapsed / (1000 * 360 * 24));
+    return daysSinceBorn;
 }
 
 export default FootballPlayer;
